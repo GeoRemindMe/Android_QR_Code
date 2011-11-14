a31f0e622f7ab46cfdb9c6831a8f730c8bc3f023
Ricardo Caballero <rcabamo@gmail.com>
2011-11-14 13:56:27 +0100

    Update

--- gen/org/georemindme/R.java
+++ gen/org/georemindme/R.java
@@ -11,24 +11,29 @@ public final class R {
     public static final class attr {
     }
     public static final class drawable {
-        public static final int icon=0x7f020000;
-        public static final int logo=0x7f020001;
+        public static final int cofre=0x7f020000;
+        public static final int icon=0x7f020001;
+        public static final int logo=0x7f020002;
     }
     public static final class id {
-        public static final int boton=0x7f050003;
-        public static final int logo=0x7f050001;
-        public static final int pista=0x7f050002;
-        public static final int scroll=0x7f050000;
+        public static final int boton=0x7f060004;
+        public static final int logo=0x7f060001;
+        public static final int pista=0x7f060003;
+        public static final int scroll=0x7f060000;
+        public static final int sigPista=0x7f060002;
     }
     public static final class layout {
         public static final int captureqr=0x7f030000;
         public static final int main=0x7f030001;
     }
+    public static final class raw {
+        public static final int bird=0x7f040000;
+    }
     public static final class string {
-        public static final int app_name=0x7f040001;
+        public static final int app_name=0x7f050001;
         /**  captureQRCodeActivity 
          */
-        public static final int dialogo=0x7f040002;
-        public static final int hello=0x7f040000;
+        public static final int dialogo=0x7f050002;
+        public static final int hello=0x7f050000;
     }
 }
